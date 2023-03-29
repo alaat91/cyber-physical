efa9728824aee555ec86117a492195302965c2a3
Parallels <balesic@student.chalmers.se>
2023-03-29 16:22:05 +0200

    Add comment for assignment 5

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "PrimeChecker.hpp"
 
+// This comment is for demonstration purpose only
 int main(int argc, char** argv){
     if(argc == 2){
         int number = std::stoi(argv[1]);
