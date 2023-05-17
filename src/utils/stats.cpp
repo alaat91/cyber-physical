820ea963951e44415e24fef65ebdab30a6abe853
balesic <balesic@student.chalmers.se>
2023-05-17 15:08:13 +0200

    Add comments and refactor variable names

--- src/utils/stats.cpp
+++ src/utils/stats.cpp
@@ -1,165 +1,151 @@
 #include "stats.hpp"
 
 void calculateStats(
-    cv::Mat imgCenterBlue,
-    cv::Mat imgCenterYellow,
-    opendlv::proxy::GroundSteeringRequest gsr,
-    bool isBlueLeft)
+        cv::Mat imgCenterBlue,                  // Input image with blue pixels in the center
+        cv::Mat imgCenterYellow,                // Input image with yellow pixels in the center
+        opendlv::proxy::GroundSteeringRequest groundSteeringReq,   // Ground steering request object
+        bool isBlueLeft                         // Flag indicating if blue is on the left side
+)
 {
 
-    int bluePixelAmount = 0;
-    int yellowPixelAmount = 0;
-    bool blue = false;
-    bool yellow = false;
-    bool gsrbool = false;
-    static int countYellow = 0;
-    static int countBlue = 0;
-    static int onlygsrblue = 0;
-    static int onlyblue = 0;
-    static int onlyyellow = 0;
-    static int onlygsryellow = 0;
-    static int emptyBlue = 0;
-    static int emptyYellow = 0;
+    int bluePixelAmount = 0;                // Variable to store the number of blue pixels
+    int yellowPixelAmount = 0;              // Variable to store the number of yellow pixels
+    bool hasBluePixels = false;             // Flag indicating presence of blue pixels
+    bool hasYellowPixels = false;           // Flag indicating presence of yellow pixels
+    bool hasValidGroundSteering = false;     // Flag indicating the validity of ground steering request
+    static int blueTurnsCorrectCount = 0;    // Counter for correct blue turns
+    static int yellowTurnsCorrectCount = 0;  // Counter for correct yellow turns
+    static int blueTurnsOnlyGSRCount = 0;    // Counter for only ground steering request turning right
+    static int blueTurnsOnlyAlgorithmCount = 0;  // Counter for only algorithm turning right
+    static int yellowTurnsOnlyAlgorithmCount = 0;  // Counter for only algorithm turning left
+    static int yellowTurnsOnlyGSRCount = 0;   // Counter for only ground steering request turning left
+    static int noBlueNoTurnsCount = 0;       // Counter for no blue pixels and no turn
+    static int noYellowNoTurnsCount = 0;     // Counter for no yellow pixels and no turn
 
-    bluePixelAmount = cv::countNonZero(imgCenterBlue);
-    // this is when the car should turn right
+    bluePixelAmount = cv::countNonZero(imgCenterBlue);   // Count the number of non-zero (blue) pixels
+
+    // Check if there are enough blue pixels to turn right
     if (bluePixelAmount > 250)
     {
-        blue = true;
+        hasBluePixels = true;                        // Set the hasBluePixels flag to true
     }
-    // this is when the car actually turns right
+
+    // Determine if the ground steering request is indicating a right turn based on the flag isBlueLeft
     if (isBlueLeft)
     {
-        if (gsr.groundSteering() < 0)
+        if (groundSteeringReq.groundSteering() < 0)       // Check if the ground steering request is negative (right turn)
         {
-            gsrbool = true;
+            hasValidGroundSteering = true;                 // Set the hasValidGroundSteering flag to true
         }
     }
     else
     {
-        if (gsr.groundSteering() > 0)
+        if (groundSteeringReq.groundSteering() > 0)       // Check if the ground steering request is positive (right turn)
         {
-            gsrbool = true;
+            hasValidGroundSteering = true;                 // Set the hasValidGroundSteering flag to true
         }
     }
-    if (blue && gsrbool)
-    {
-        countBlue++;
-    }
-    if (blue && !gsrbool)
-    {
-        onlyblue++;
-    }
-    if (!blue && gsrbool)
-    {
-        onlygsrblue++;
-    }
-    if (!blue && !gsrbool)
-    {
-        emptyBlue++;
-    }
-    // Our algorithm turns the car when it should
-    //std::cout << "Correct blue  : ";
-    //std::cout << countBlue << std::endl;
-    // Our algorithm turns the car when it shouldn’t
-    //std::cout << "onlyBlue      : ";
-    //std::cout << onlyblue << std::endl;
-    // Our algorithm doesn’t turn the car when it should
-    //std::cout << "only Gsr right: ";
-    //std::cout << onlygsrblue << std::endl;
-    // Our algorithm doesn’t turn the car, and it shouldn’t
-    //std::cout << "Neither output: ";
-    //std::cout << emptyBlue << std::endl;
 
-    yellowPixelAmount = cv::countNonZero(imgCenterYellow);
+    // Increment the respective counters based on the conditions
+    if (hasBluePixels && hasValidGroundSteering)
+    {
+        blueTurnsCorrectCount++;                        // Increment the counter for correct blue turns
+    }
+    if (hasBluePixels && !hasValidGroundSteering)
+    {
+        blueTurnsOnlyAlgorithmCount++;                   // Increment the counter for only algorithm turning right
+    }
+    if (!hasBluePixels && hasValidGroundSteering)
+    {
+        blueTurnsOnlyGSRCount++;                         // Increment the counter for only ground steering request turning right
+    }
+    if (!hasBluePixels && !hasValidGroundSteering)
+    {
+        noBlueNoTurnsCount++;                             // Increment the counter for no blue pixels and no turn
+    }
 
-    // this is when the car should turn left
+    yellowPixelAmount = cv::countNonZero(imgCenterYellow);   // Count the number of non-zero (yellow) pixels
+
+    // Check if there are enough yellow pixels to turn left
     if (yellowPixelAmount > 250)
     {
-        yellow = true;
+        hasYellowPixels = true;                           // Set the hasYellowPixels flag to true
     }
 
-    gsrbool = false;
+    hasValidGroundSteering = false;                         // Reset the ground steering request flag
 
+    // Determine if the ground steering request is indicating a left turn based on the flag isBlueLeft
     if (isBlueLeft)
     {
-        if (gsr.groundSteering() > 0)
+        if (groundSteeringReq.groundSteering() > 0)       // Check if the ground steering request is positive (left turn)
         {
-            gsrbool = true;
+            hasValidGroundSteering = true;                 // Set the hasValidGroundSteering flag to true
         }
     }
     else
     {
-        if (gsr.groundSteering() < 0)
+        if (groundSteeringReq.groundSteering() < 0)       // Check if the ground steering request is negative (left turn)
         {
-            gsrbool = true;
+            hasValidGroundSteering = true;                 // Set the hasValidGroundSteering flag to true
         }
     }
 
-    if (yellow && gsrbool)
+    // Increment the respective counters based on the conditions
+    if (hasYellowPixels && hasValidGroundSteering)
     {
-        countYellow++;
+        yellowTurnsCorrectCount++;                        // Increment the counter for correct yellow turns
     }
-    if (yellow && !gsrbool)
+    if (hasYellowPixels && !hasValidGroundSteering)
     {
-        onlyyellow++;
+        yellowTurnsOnlyAlgorithmCount++;                   // Increment the counter for only algorithm turning left
     }
-    if (!yellow && gsrbool)
+    if (!hasYellowPixels && hasValidGroundSteering)
     {
-        onlygsryellow++;
+        yellowTurnsOnlyGSRCount++;                         // Increment the counter for only ground steering request turning left
     }
-    if (!yellow && !gsrbool)
+    if (!hasYellowPixels && !hasValidGroundSteering)
     {
-        emptyYellow++;
+        noYellowNoTurnsCount++;                             // Increment the counter for no yellow pixels and no turn
     }
-    // Our algorithm turns the car when it should
-    //std::cout << "Correct yellow: ";
-    //std::cout << countYellow << std::endl;
-    // Our algorithm turns the car when it shouldn’t
-    //std::cout << "onlyYellow    : ";
-    //std::cout << onlyyellow << std::endl;
-    // Our algorithm doesn’t turn the car when it should
-    //std::cout << "only Gsr left : ";
-    //std::cout << onlygsryellow << std::endl;
-    // Our algorithm doesn’t turn the car, and it shouldn’t
-    std::cout << "Neither output: ";
-    std::cout << emptyYellow << std::endl;
 
-    yellow = false;
-    gsrbool = false;
+    hasYellowPixels = false;                         // Reset the yellow flag
+    hasValidGroundSteering = false;                   // Reset the ground steering request flag
 }
 
-void writePixels(float bluePixels, float YellowPixels, float gsr, float calcGsr)
+void writePixels(float bluePixels, float yellowPixels, float groundSteeringReq, float calcGroundSteering)
 {
-    std::ofstream myfile;
-    myfile.open("/host/res.csv", std::ios_base::app);
-    myfile << bluePixels << "," << YellowPixels << "," << gsr << "," << calcGsr << "\n";
-    myfile.close();
+    std::ofstream outputFile;
+    outputFile.open("/host/res.csv", std::ios_base::app);   // Open the file in append mode
+    outputFile << bluePixels << "," << yellowPixels << "," << groundSteeringReq << "," << calcGroundSteering << "\n";   // Write the pixel and steering values to the file
+    outputFile.close();                                     // Close the file
 }
 
-void determineError(float g1, float g2)
+void determineError(float groundSteeringReq1, float groundSteeringReq2)
 {
-    static float totalFrames = 0;
-    static float correctFrames = 0;
+    static float totalFrames = 0;                     // Counter for total frames
+    static float correctFrames = 0;                   // Counter for correct frames
 
-    float e = ((fabs(g1 - g2)) / fabs(g1)) * 100;
+    float error = ((fabs(groundSteeringReq1 - groundSteeringReq2)) / fabs(groundSteeringReq1)) * 100;   // Calculate the error percentage
 
-    if (g1 == 0)
+    if (groundSteeringReq1 == 0)
     {
-        e = fabs(g1 - g2);
+        error = fabs(groundSteeringReq1 - groundSteeringReq2);    // Set the error to the absolute difference when groundSteeringReq1 is zero
     }
-    std::cout << "groundSteeringRequest: " << g1 << std::endl;
-    std::cout << "steeringWheelAngle: " << g2 << std::endl;
-    std::cout << "Error: " << e << std::endl;
+    std::cout << "groundSteeringRequest: " << groundSteeringReq1 << std::endl;    // Print the original ground steering request
+    std::cout << "steeringWheelAngle: " << groundSteeringReq2 << std::endl;        // Print the calculated steering value
+    std::cout << "Error: " << error << std::endl;                     // Print the error value
 
-    if (g1 != 0 && e <= 30)
+    // Check if the error is within the acceptable range and increment the correctFrames counter accordingly
+    if (groundSteeringReq1 != 0 && error <= 30)
     {
-        correctFrames++;
+        correctFrames++;                    // Increment the counter for correct steering frames
     }
-    else if (g1 == 0 && fabs(g1 - g2) <= 0.05)
+    else if (groundSteeringReq1 == 0 && fabs(groundSteeringReq1 - groundSteeringReq2) <= 0.05)
     {
-        correctFrames++;
+        correctFrames++;                    // Increment the counter for correct steering frames when the ground steering request is zero
     }
-    totalFrames++;
-    std::cout << "Frame Stats:" << correctFrames << "/" << totalFrames << "\t" << ((correctFrames / totalFrames) * 100) << " %" << std::endl;
+
+    totalFrames++;                          // Increment the total frames counter
+
+    std::cout << "Frame Stats:" << correctFrames << "/" << totalFrames << "\t" << ((correctFrames / totalFrames) * 100) << " %" << std::endl;   // Print the frame statistics
 }
